c8486a481611296e8d22bf851cc2f9122ec81aae
James Wang <james.wang@sage.com>
2022-02-25 16:14:06 -0800

    updates

--- code/1-avro-java/avro-tools.sh
+++ code/1-avro-java/avro-tools.sh
@@ -1,9 +1,11 @@
 # put this in any directory you like
-wget http://central.maven.org/maven2/org/apache/avro/avro-tools/1.8.2/avro-tools-1.8.2.jar
+wget https://repo1.maven.org/maven2/org/apache/avro/avro-tools/1.11.0/avro-tools-1.11.0.jar
 
 # run this from our project folder. Make sure ~/avro-tools-1.8.2.jar is your actual avro tools location
-java -jar ~/avro-tools-1.8.2.jar tojson --pretty customer-generic.avro 
-java -jar ~/avro-tools-1.8.2.jar tojson --pretty customer-specific.avro 
+java -jar ~/avro-tools-1.11.0.jar # help info
+java -jar ~/avro-tools-1.11.0.jar tojson --pretty customer-generic.avro
+java -jar ~/avro-tools-1.11.0.jar tojson --pretty customer-specific.avro
 
 # getting the schema
-java -jar ~/avro-tools-1.8.2.jar getschema customer-specific.avro 
+java -jar ~/avro-tools-1.11.0.jar getschema customer-generic.avro
+java -jar ~/avro-tools-1.11.0.jar getschema customer-specific.avro
